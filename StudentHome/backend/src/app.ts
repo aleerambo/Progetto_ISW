32eaf958114f939e671c594d44db15cd6a1fa774
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2024-12-19 17:26:20 +0100

    ADD connect-history-api-fallback packages and UPDATE app.ts configuration

--- StudentHome/backend/src/app.ts
+++ StudentHome/backend/src/app.ts
@@ -1,3 +1,23 @@
-console.log("Cioccolato")
+import express, { Express } from "express"
+import history from "connect-history-api-fallback"
+
+const app: Express = express()
+const port: number = 3000
+
+app.use(history())
+app.use(express.static("public"))
+app.use(express.static("dist-frontend"))
+
+//app.use(articoliRouter)
+
+
+app.use(function(req, res, next) {
+  res.setHeader("Content-Type", "text/plain")
+  res.status(404).send("Ops... Pagina non trovata")
+})
+
+app.listen(port, function() {
+  console.log(`Listening on http://localhost:${port}`)
+})
 
 //Finito il progetto esegui a tarminale "npm run build" per la conversione in js
