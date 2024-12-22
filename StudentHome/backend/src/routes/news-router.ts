54fe4c7a60a99881bf7d2aa35a2fd3d5de30d2c3
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2024-12-22 12:22:09 +0100

    UPDATE news-router with correction on path for news/:n

--- StudentHome/backend/src/routes/news-router.ts
+++ StudentHome/backend/src/routes/news-router.ts
@@ -4,6 +4,6 @@ import * as articoliController from "../controllers/news-controller"
 const router: Router = Router()
 
 router.get("/api/news", articoliController.allNews)
-router.get("/api/news /:n", articoliController.lastNNews)
+router.get("/api/news/:n", articoliController.lastNNews)
 
 export default router
