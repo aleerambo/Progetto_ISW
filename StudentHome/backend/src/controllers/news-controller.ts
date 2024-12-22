d791166de56dbb61b781d605fc9a13080d794f86
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2024-12-22 12:09:44 +0100

    UPDATE news-controller with new export function last n news

--- StudentHome/backend/src/controllers/news-controller.ts
+++ StudentHome/backend/src/controllers/news-controller.ts
@@ -12,3 +12,14 @@ export async function allNews(req: Request, res: Response) {
     )
 }
 
+export async function lastNNews(req: Request, res: Response) {
+    connection.execute(
+      `SELECT * 
+       FROM news
+       ORDER BY data_pubblicazione DESC LIMIT ?`,
+      [req.params.n],
+      function (err, results, fields) {
+        res.json(results)
+      }
+    )
+}
