43bf3f0a5ad3a9346c89a6521b4e168f32eed1bb
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2024-12-22 12:08:55 +0100

    UPDATE news-controller with new export all news function

--- StudentHome/backend/src/controllers/news-controller.ts
+++ StudentHome/backend/src/controllers/news-controller.ts
@@ -1,2 +1,14 @@
 import { Request, Response } from "express"
 import { connection } from "../utils/db"
+
+export async function allNews(req: Request, res: Response) {
+    connection.execute(
+      `SELECT * 
+       FROM news`,
+      [],
+      function (err, results, fields) {
+        res.json(results)
+      }
+    )
+}
+
