71ca3f6ecb67fdd956f8d0afc3e76598a20a9feb
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-01-03 18:44:40 +0100

    UPDATE auth-controllers adding any conversion to avoid mismatch

--- StudentHome/backend/src/controllers/auth-controller.ts
+++ StudentHome/backend/src/controllers/auth-controller.ts
@@ -15,7 +15,7 @@ export const register = async (req: Request, res: Response) => {
   const { mail, password } = req.body
 
   // Verifica che la mail sia disponibile
-  const [users] = await connection.execute("SELECT mail FROM utente WHERE mail=?", [
+  const [users]:any = await connection.execute("SELECT mail FROM utente WHERE mail=?", [
     mail,
   ])
 
@@ -34,7 +34,7 @@ export const register = async (req: Request, res: Response) => {
   ])
 
   // Estrae i dati per il nuovo utente
-  const [results] = await connection.execute(
+  const [results]:any = await connection.execute(
     "SELECT id, mail, role FROM utente WHERE mail=?",
     [mail]
   )
@@ -58,7 +58,7 @@ export const login = async (req: Request, res: Response) => {
   const { mail, password } = req.body
 
   // Esegue la query al database per ottenere i dati dell'utente in base alla mail
-  const [results] = await connection.execute(
+  const [results]:any = await connection.execute(
     "SELECT id, mail, password, role FROM utente WHERE mail=?",
     [mail]
   )
