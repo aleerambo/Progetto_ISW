9b0482b929f2f3e3b27f465cb657b625a49ea460
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-01-03 18:47:10 +0100

    UPDATE with page implementation

--- StudentHome/frontend/src/pages/Login.vue
+++ StudentHome/frontend/src/pages/Login.vue
@@ -1,3 +1,52 @@
+<script lang="ts">
+import axios from "axios"
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  data() {
+    return {
+      mail: "",
+      password: "",
+    }
+  },
+  methods: {
+    async onSubmit() {
+      try {
+        await axios.post("/api/auth/login", {
+          mail: this.mail,
+          password: this.password,
+        })
+        location.href = "/"
+      } catch (e: any) {
+        if (e.response) {
+          alert(`${e.response.status} - ${e.response.statusText}\n${e.response.data}`)
+        } else {
+          alert(e.message)
+        }
+      }
+    },
+  },
+})
+</script>
+
 <template>
-    <a class="btn btn-primary" href="#" role="button">ACCEDI</a>
+  <div class="prose dark:prose-invert">
+    <h1>Login</h1>
+    <form class="not-prose flex flex-col gap-3" @submit.prevent="onSubmit">
+      <input
+        type="text"
+        v-model="mail"
+        class="rounded-lg bg-transparent border-zinc-200 dark:border-zinc-700"
+        placeholder="Mail"
+      />
+      <input
+        type="password"
+        v-model="password"
+        class="rounded-lg bg-transparent border-zinc-200 dark:border-zinc-700"
+        placeholder="Password"
+      />
+      <button type="submit" class="btn btn-primary w-1/2 mx-auto mt-3">Accedi</button>
+    </form>
+    <p>Non sei ancora iscritto? Clicca qui e <RouterLink to="/registrazione">REGISTRATI</RouterLink></p>
+  </div>
 </template>
