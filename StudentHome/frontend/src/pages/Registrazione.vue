7f890b9d69bf1b4a83475b297c3c184c58b78797
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-01-03 17:39:43 +0100

    ADD Regitrazione page

---
+++ StudentHome/frontend/src/pages/Registrazione.vue
@@ -0,0 +1,64 @@
+<script lang="ts">
+import axios from "axios"
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  data() {
+    return {
+      mail: "",
+      password: "",
+      confirmPassword: "",
+    }
+  },
+  methods: {
+    async onSubmit() {
+      if (this.password != this.confirmPassword) {
+        alert("Le due password non coincidono.")
+        return
+      }
+      try {
+        await axios.post("/api/auth/register", {
+          mail: this.mail,
+          password: this.password,
+        })
+        location.href = "/"
+      } catch (e: any) {
+        if (e.response) {
+          alert(`${e.response.status} - ${e.response.statusText}\n${e.response.data}`)
+        } else {
+          alert(e.message)
+        }
+      }
+    },
+  },
+})
+</script>
+
+<template>
+  <div class="prose dark:prose-invert">
+    <h1>Registrazione</h1>
+    <form class="not-prose flex flex-col gap-3" @submit.prevent="onSubmit">
+      <input
+        type="text"
+        v-model="mail"
+        class="rounded-lg bg-transparent border-zinc-200 dark:border-zinc-700"
+        placeholder="Mail"
+      />
+      <input
+        type="password"
+        v-model="password"
+        class="rounded-lg bg-transparent border-zinc-200 dark:border-zinc-700"
+        placeholder="Password"
+      />
+      <input
+        type="password"
+        v-model="confirmPassword"
+        class="rounded-lg bg-transparent border-zinc-200 dark:border-zinc-700"
+        placeholder="Conferma password"
+      />
+      <button type="submit" class="btn !bg-blue-500 text-white w-1/2 mx-auto mt-3">
+        Registrati
+      </button>
+    </form>
+  </div>
+</template>
