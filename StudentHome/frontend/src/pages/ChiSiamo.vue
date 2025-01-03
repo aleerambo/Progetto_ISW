68666abf66d6fef2f8fea2df5c94f5bc4d6b2f0f
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-01-03 15:57:35 +0100

    UPDATE ChiSiamo page content

--- StudentHome/frontend/src/pages/ChiSiamo.vue
+++ StudentHome/frontend/src/pages/ChiSiamo.vue
@@ -1,11 +1,23 @@
 <template>
-    <p>
-        Benvenuto su StudentHome! 
+    <div class="container my-5">
+        <div class="row align-items-center">
+            <!-- Colonna per il testo -->
+            <div class="col-12 col-md-6 order-2 order-md-1">
+                <h1 class="mb-4">Chi siamo</h1>
+                <p>
+                    Benvenuto su StudentHome! 
 
-        Siamo una piattaforma creata da studenti per studenti, con l'obiettivo di semplificare la ricerca di alloggi universitari. Il nostro sogno? Creare un punto di riferimento unico per trovare appartamenti, camere e posti letto in modo rapido, sicuro e affidabile.
+                    Siamo una piattaforma creata da studenti per studenti, con l'obiettivo di semplificare la ricerca di alloggi universitari. Il nostro sogno? Creare un punto di riferimento unico per trovare appartamenti, camere e posti letto in modo rapido, sicuro e affidabile.
 
-        Su StudentHome troverai solo inserzioni pensate per le esigenze degli studenti, con offerte speciali e recensioni autentiche per aiutarti a fare la scelta giusta. Crediamo nel potere della condivisione e della trasparenza: per questo mettiamo al centro una community attiva, dove chi cerca casa e chi la offre può entrare in contatto diretto.
+                    Su StudentHome troverai solo inserzioni pensate per le esigenze degli studenti, con offerte speciali e recensioni autentiche per aiutarti a fare la scelta giusta. Crediamo nel potere della condivisione e della trasparenza: per questo mettiamo al centro una community attiva, dove chi cerca casa e chi la offre può entrare in contatto diretto.
 
-        Che tu stia cercando il tuo primo appartamento o una stanza accogliente vicino all’università, siamo qui per rendere il processo semplice e senza stress. StudentHome è la tua nuova casa... prima della tua prossima casa!
-    </p>
+                    Che tu stia cercando il tuo primo appartamento o una stanza accogliente vicino all’università, siamo qui per rendere il processo semplice e senza stress. StudentHome è la tua nuova casa... prima della tua prossima casa!
+                </p>
+            </div>
+            <!-- Colonna per l'immagine -->
+            <div class="col-12 col-md-6 order-1 order-md-2 text-center">
+                <img src="..\..\public\img\scontornato.png" class="img-fluid" alt="...">
+            </div>
+        </div>   
+    </div>
 </template>
