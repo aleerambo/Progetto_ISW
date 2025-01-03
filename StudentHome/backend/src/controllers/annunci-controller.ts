5b586a72bf0974e3a71a7ad120d74eea004ea85d
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-01-03 17:21:43 +0100

    UPDATE annunci-controller with new creation and delation function

--- StudentHome/backend/src/controllers/annunci-controller.ts
+++ StudentHome/backend/src/controllers/annunci-controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import { connection } from "../utils/db"
+import { getUser } from "../utils/auth"
 
 export async function allAnnunci(req: Request, res: Response) {
     connection.execute(
@@ -291,4 +292,45 @@ export async function AnnunciUtente(req: Request, res: Response) {
       res.json(results)
     }
   )
+}
+
+export const createAnnuncio = async (req: Request, res: Response) => {
+  // Verifica che l'utente abbia effettuato il login
+  const user = getUser(req, res)
+  if (!user) {
+    res.status(401).send("Questa operazione richiede l'autenticazione.")
+    return
+  }
+
+  await connection.execute("INSERT INTO annuncio (utente_id, prezzo, descrizione, locali, mq, piano, indirizzo) VALUES (?, ?, ?, ?, ?, ?, ?)", [
+    user.id,
+    req.body.content,
+  ])
+  res.json({ success: true })
+}
+
+export const deleteAnnuncio = async (req: Request, res: Response) => {
+  // Verifica che l'utente abbia effettuato il login
+  const user = getUser(req, res)
+  if (!user) {
+    res.status(401).send("Questa operazione richiede l'autenticazione.")
+    return
+  }
+
+  // Verifica che l'annuncio esista
+  const [annunci] = await connection.execute("SELECT * FROM annuncio WHERE id=?", [req.params.id])
+  if (!Array.isArray(annunci) || annunci.length == 0) {
+    res.status(404).send("Annuncio non trovato.")
+    return
+  }
+
+  // Verifica che l'utente abbia i permessi per eliminare il post
+  const annuncio = annunci[0] as any
+  if (annuncio.utente_id != user.id && user.role != "admin") {
+    res.status(403).send("Non hai i permessi per eliminare questo post.")
+    return
+  }
+
+  await connection.execute("DELETE FROM annuncio WHERE id=?", [req.params.id])
+  res.json({ success: true })
 }
