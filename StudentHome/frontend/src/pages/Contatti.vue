2823b2ba3c0a8aa0dffbd5e4c3c3625249fbf688
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-01-13 18:36:48 +0100

    UPDATE Contatti page with lots of p

--- StudentHome/frontend/src/pages/Contatti.vue
+++ StudentHome/frontend/src/pages/Contatti.vue
@@ -8,19 +8,26 @@
             <li class="breadcrumb-item active" aria-current="page">Contatti</li>
         </ol>
     </nav>    
-    <div>
-        <h1 class="p-2 mb-2 bg-secondary-subtle text-center" style="color: #1E3A8A;">
-            Contatti
-        </h1>
-        <p>Hai bisogno di assistenza o vuoi metterti in contatto con noi? Siamo qui per aiutarti!
-
+    <h1 class="p-2 mb-2 bg-secondary-subtle text-center" style="color: #1E3A8A;">
+        Contatti
+    </h1>
+    <div class="p-3">
+        <p>
+            Hai bisogno di assistenza o vuoi metterti in contatto con noi? Siamo qui per aiutarti!
+        </p>
+        <p>
             Puoi scriverci una mail all’indirizzo:
-
-            studenthome@support.com
+        </p>
+        <p>
+            <span class="fw-bold"><a href="mailto:studenthome@support.com">studenthome@support.com</a></span>
+        </p>
+        <p>
             Oppure contattarci telefonicamente al numero:
-
-            051 526398
-
+        </p>
+        <p>
+            <span class="fw-bold"><a href="tel:051526398">051 526398</a></span>
+        </p>
+        <p>
             Il nostro team è disponibile per rispondere alle tue domande, chiarire dubbi e fornirti il supporto necessario per trovare la casa perfetta per i tuoi studi. 
             Non esitare a contattarci, siamo al tuo fianco
         </p>
