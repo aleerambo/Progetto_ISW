268eec599cd5becef98ee576f21adb9046b0a074
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-01-13 10:23:34 +0100

    UPDATE vite.config.ts with proxy

--- StudentHome/frontend/vite.config.ts
+++ StudentHome/frontend/vite.config.ts
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    // Reindirizza al backend le richieste che iniziano con /api
+    proxy: {
+      "/api": "http://localhost:3000",
+    },
+  },
 })
