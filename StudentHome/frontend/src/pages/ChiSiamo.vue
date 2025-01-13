d683986447e6b62bf3fa5bf9db5e47763d542179
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-01-13 18:36:25 +0100

    UPDATE body content of ChiSiamo page with lots of p and title under image

--- StudentHome/frontend/src/pages/ChiSiamo.vue
+++ StudentHome/frontend/src/pages/ChiSiamo.vue
@@ -11,20 +11,24 @@
     <div class="container my-5">
         <div class="row align-items-center">
             <!-- Colonna per il testo -->
-            <div class="col-12 col-md-6 order-2 order-md-1">
+            <div class="col-12 col-md-6 order-2 order-md-1 fs-5">
                 <p>
-                    Benvenuto su StudentHome! 
-
+                    Benvenuto su <span class="fw-bold">StudentHome</span>! 
+                </p>
+                <p>
                     Siamo una piattaforma creata da studenti per studenti, con l'obiettivo di semplificare la ricerca di alloggi universitari. Il nostro sogno? Creare un punto di riferimento unico per trovare appartamenti, camere e posti letto in modo rapido, sicuro e affidabile.
-
+                </p>
+                <p>
                     Su StudentHome troverai solo inserzioni pensate per le esigenze degli studenti, con offerte speciali e recensioni autentiche per aiutarti a fare la scelta giusta. Crediamo nel potere della condivisione e della trasparenza: per questo mettiamo al centro una community attiva, dove chi cerca casa e chi la offre può entrare in contatto diretto.
-
+                </p>
+                <p>
                     Che tu stia cercando il tuo primo appartamento o una stanza accogliente vicino all’università, siamo qui per rendere il processo semplice e senza stress. StudentHome è la tua nuova casa... prima della tua prossima casa!
                 </p>
             </div>
             <!-- Colonna per l'immagine -->
             <div class="col-12 col-md-6 order-1 order-md-2 text-center">
                 <img src="/img/scontornato.png" class="img-fluid" alt="Logo StudentHome">
+                <h2 class="mt-3">StudentHome</h2>
             </div>
         </div>   
     </div>
