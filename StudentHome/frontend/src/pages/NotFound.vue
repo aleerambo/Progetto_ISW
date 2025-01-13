b56ab06114869bf38cfe872bf89d2f6f5d55b8cc
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-01-13 14:59:38 +0100

    UPDATE change path of moved images

--- StudentHome/frontend/src/pages/NotFound.vue
+++ StudentHome/frontend/src/pages/NotFound.vue
@@ -1,5 +1,5 @@
 <template>
-    <img class="rounded mx-auto d-block" src="/img404.webp" alt="Immagine pagina 404" width="300" height="300" id="Img404">
+    <img class="rounded mx-auto d-block" src="/img/img404.webp" alt="Immagine pagina 404" width="300" height="300" id="Img404">
     <p class="h2 text-center">Ops! Pagina non trovata</p>
 </template>
   
