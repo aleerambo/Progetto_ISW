e1f3a1a1a18ba32d616dabbd50a276cd115543db
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-01-13 18:37:34 +0100

    UPDATE GuidaAlloggio with font size adjustments

--- StudentHome/frontend/src/pages/GuidaAlloggio.vue
+++ StudentHome/frontend/src/pages/GuidaAlloggio.vue
@@ -13,28 +13,28 @@
         <div class="row align-items-center">
             <!-- Colonna per il testo -->
             <div class="col-12 col-md-6 order-2 order-md-1">
-                <p>
+                <p class="fs-5">
                     Trovare l’alloggio perfetto può sembrare una sfida, ma con alcuni consigli mirati puoi semplificare il processo e fare la scelta giusta per le tue esigenze. <br/>
                     Noi di StudentHome, siamo qui per aiutarti a trovare la tua prossima casa con facilità.
                 </p>
                 <h2>1. Definisci le tue priorità</h2>
-                <p>
+                <p class="fs-5">
                     Prima di iniziare, elenca le tue necessità: vicinanza all’università, budget, numero di coinquilini e servizi indispensabili (es. Wi-Fi, lavatrice, aria condizionata).
                 </p>
                 <h2>2. Esplora le opzioni</h2>
-                <p>
+                <p class="fs-5">
                     Le foto e le descrizioni dettagliate ti aiuteranno a farti un’idea chiara.
                 </p>
                 <h2>3. Valuta il quartiere</h2>
-                <p>
+                <p class="fs-5">
                     Scegli un quartiere sicuro e ben collegato con i mezzi pubblici. Leggi le recensioni degli altri studenti per avere un’idea della zona.
                 </p>
                 <h2>4. Visita di persona (se puoi)</h2>
-                <p>
+                <p class="fs-5">
                     Le foto sono utili, ma una visita dal vivo ti permetterà di verificare ogni dettaglio e percepire l’atmosfera del luogo. Non trascurare aspetti come la luminosità, lo stato degli arredi e l’isolamento acustico.
                 </p>
                 <h2>5. Prendi una decisione consapevole</h2>
-                <p>
+                <p class="fs-5">
                     Una volta trovato l’alloggio che risponde alle tue esigenze, non aspettare troppo a lungo. Il mercato degli affitti per studenti è molto dinamico, e la tua futura casa potrebbe andare via velocemente!
                 </p>
             </div>
