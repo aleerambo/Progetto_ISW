2bbdca8c3acd6545be7c6dce2b7697587e02491b
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-01-15 17:42:50 +0100

    UPDATE add a control if the user dosen't have announces

--- StudentHome/frontend/src/pages/Utente.vue
+++ StudentHome/frontend/src/pages/Utente.vue
@@ -22,15 +22,20 @@ export default defineComponent({
 <template>
   <div>
     <h1>I miei annunci</h1>
-    <div v-for="annuncio in annunci" :key="annuncio.id" class="card mb-4 shadow-sm">
-      <div class="card-body">
-        <h5 class="card-title">{{ annuncio.descrizione }}</h5>
-        <p class="card-text">{{ annuncio.prezzo }} €/mese</p>
-        <p class="card-text">{{ annuncio.locali }} locali</p>
-        <p class="card-text">{{ annuncio.mq }} mq</p>
-        <p class="card-text">{{ annuncio.piano }} piano</p>
-        <p class="card-text">{{ annuncio.servizi }}</p>
-      </div>
+    <div v-if="annunci.length === 0" class="h2 bg-secondary text-white-50 text-center">
+      Non hai ancora pubblicato alcun annuncio!
     </div>
+    <template v-else>
+        <div v-for="annuncio in annunci" :key="annuncio.id" class="card mb-4 shadow-sm">
+            <div class="card-body">
+                <h5 class="card-title">{{ annuncio.descrizione }}</h5>
+                <p class="card-text">{{ annuncio.prezzo }} €/mese</p>
+                <p class="card-text">{{ annuncio.locali }} locali</p>
+                <p class="card-text">{{ annuncio.mq }} mq</p>
+                <p class="card-text">{{ annuncio.piano }} piano</p>
+                <p class="card-text">{{ annuncio.servizi }}</p>
+            </div>
+        </div>
+    </template>
   </div>
 </template>
