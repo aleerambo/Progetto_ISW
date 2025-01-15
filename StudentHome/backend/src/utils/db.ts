ff4561b9c28b27c9ed62624ba4e7cb0e65ad9054
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-01-15 09:32:00 +0100

    UPDATE adjust some indentation

--- StudentHome/backend/src/utils/db.ts
+++ StudentHome/backend/src/utils/db.ts
@@ -10,8 +10,8 @@ export const connection: Connection = mysql.createConnection({
 // Tentativo di connessione
 connection.connect((err) => {
   if (err) {
-      console.error('Database connection failed:', err.message);
-      return;
+    console.error('Database connection failed:', err.message);
+    return;
   }
   console.log('Connected to the MySQL database.');
 });
