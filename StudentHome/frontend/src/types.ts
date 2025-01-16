ddd9faaa77e6b31d4160bf17a873cdf71273a519
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-01-16 11:16:39 +0100

    UPDATE add new interfaces for Quartiere and Servizio

--- StudentHome/frontend/src/types.ts
+++ StudentHome/frontend/src/types.ts
@@ -38,3 +38,14 @@ export interface Annuncio {
   distanza?: string;
   indirizzo: string;
 }
+
+export interface Quartiere {
+  id: number;
+  nome_quartiere: string;
+  descrizione: string;
+}
+
+export interface Servizio {
+  id: number;
+  nome_servizio: string;
+}
