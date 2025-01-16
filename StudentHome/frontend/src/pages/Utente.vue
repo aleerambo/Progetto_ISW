86aa34acf21645bbfbfbb925a92d395f6c447dd5
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-01-16 15:31:59 +0100

    UPDATE add button delete to personal list of announces

--- StudentHome/frontend/src/pages/Utente.vue
+++ StudentHome/frontend/src/pages/Utente.vue
@@ -8,7 +8,7 @@ export default defineComponent({
     user: {
       type: Object as PropType<User>,
       required: true
-    }
+    },
   },
   data() {
     return {
@@ -26,26 +26,54 @@ export default defineComponent({
     inserisciAnnuncio() {
       this.$router.push({ path: "/inserimento-annuncio" });
     },
+    async deleteAnnuncio(annuncioID: number) {
+      const confirmed = confirm("Sei sicuro di voler eliminare questo annuncio?");
+      if (!confirmed) {
+        return;
+      }
+      
+      try {
+        await axios.delete(`/api/annunci/delete/${annuncioID}`);
+        this.$emit("delete")
+        // Ricarica la pagina dopo aver eliminato l'annuncio
+        location.reload();
+      } catch (e: any) {
+        if (e.response) {
+          alert(`${e.response.status} - ${e.response.statusText}\n${e.response.data}`)
+        } else {
+          alert(e.message)
+        }
+      }
+    },
   },
 });
 </script>
 
 <template>
-  <div>
-    <h1>I miei annunci</h1>
+  <h1 class="p-2 my-4 bg-secondary-subtle text-center" style="color: #1E3A8A;">I miei annunci attivi</h1>
+  <div class="mx-3">
     <button @click="inserisciAnnuncio" class="btn btn-primary mb-4">Inserisci nuovo annuncio</button>
-    <div v-if="annunci.length === 0" class="h2 bg-secondary text-white-50 text-center">
+    <div v-if="annunci.length === 0" class="h2 bg-secondary text-white-50 text-center mt-4">
       Non hai ancora pubblicato alcun annuncio!
     </div>
     <template v-else>
         <div v-for="annuncio in annunci" :key="annuncio.id" class="card mb-4 shadow-sm">
-            <div class="card-body">
+          <div class="row g-0">
+              <!-- Sezione Immagini -->
+              <div class="col-md-4">
+                  <img :src="'img/' + annuncio.foto_annuncio || 'img/placeholder.jpg'" class="img-fluid rounded-start" alt="Immagine principale annuncio" width="300px" height="300px" />
+              </div>
+            <div class="card-body col-md-5">
                 <h5 class="card-title">{{ annuncio.descrizione }}</h5>
                 <p class="card-text">{{ annuncio.prezzo }} €/mese</p>
                 <p class="card-text">{{ annuncio.locali }} locali</p>
                 <p class="card-text">{{ annuncio.mq }} mq</p>
                 <p class="card-text">{{ annuncio.piano }} piano</p>
                 <p class="card-text">{{ annuncio.servizi }}</p>
+              </div>
+            </div>
+            <div class="position-absolute top-0 end-0">
+              <button class="btn btn-danger m-3" @click="deleteAnnuncio(annuncio.id)">Elimina</button>
             </div>
         </div>
     </template>
