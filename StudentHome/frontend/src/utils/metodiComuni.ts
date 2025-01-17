caae44b318a9904f9f27449febe417fd26829348
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-01-17 15:57:39 +0100

    UPDATE add grtImageUrl method

--- StudentHome/frontend/src/utils/metodiComuni.ts
+++ StudentHome/frontend/src/utils/metodiComuni.ts
@@ -10,21 +10,25 @@ export function ConvertiDataTesto(dateString: string): string {
 }
   
 export function CreaUrlMaps(indirizzo: string): string {
-    const baseUrl = "https://www.google.com/maps/dir/?api=1&origin=";
-    const destUrl = "Cesena+FC&destination=via+dell%27università%2B+50%2C+cesena%2C+FC&travelmode=bicycling";
-    const encodeIndirizzo = encodeURIComponent(indirizzo)
-        .replace(/%20/g, "+"); 
-    /*const encodeIndirizzo=indirizzo
-    //.replace(/\s/g, "+")
-    //.replace(/%20/g, "+")
-    .replace(/,/g, "%2C")
-    .replace(/'/g, "%27")
-    .replace(/à/g, "%C3%A0")      // à
-    .replace(/è/g, "%C3%A8")      // è
-    .replace(/é/g, "%C3%A9")      // é
-    .replace(/ì/g, "%C3%AC")      // ì
-    .replace(/ò/g, "%C3%B2")      // ò
-    .replace(/ù/g, "%C3%B9");      // ù
-    */
-    return baseUrl + encodeIndirizzo + destUrl
-  }
+  const baseUrl = "https://www.google.com/maps/dir/?api=1&origin=";
+  const destUrl = "Cesena+FC&destination=via+dell%27università%2B+50%2C+cesena%2C+FC&travelmode=bicycling";
+  const encodeIndirizzo = encodeURIComponent(indirizzo)
+      .replace(/%20/g, "+"); 
+  /*const encodeIndirizzo=indirizzo
+  //.replace(/\s/g, "+")
+  //.replace(/%20/g, "+")
+  .replace(/,/g, "%2C")
+  .replace(/'/g, "%27")
+  .replace(/à/g, "%C3%A0")      // à
+  .replace(/è/g, "%C3%A8")      // è
+  .replace(/é/g, "%C3%A9")      // é
+  .replace(/ì/g, "%C3%AC")      // ì
+  .replace(/ò/g, "%C3%B2")      // ò
+  .replace(/ù/g, "%C3%B9");      // ù
+  */
+  return baseUrl + encodeIndirizzo + destUrl
+}
+
+export function getImageUrl(foto_annuncio: string | null): string {
+  return foto_annuncio ? `/api/uploads/${foto_annuncio}` : 'img/placeholder.jpg'
+}
