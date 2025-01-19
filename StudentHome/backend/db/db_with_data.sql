16e8540097f0515eb210accf3c13b43990741a22
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2025-01-19 16:20:09 +0100

    DELETE Remove unused SQL dump files and add .gitignore for node_modules

--- StudentHome/backend/db/db_with_data.sql
+++ StudentHome/backend/db/db_with_data.sql
@@ -41,7 +41,7 @@ CREATE TABLE `annuncio` (
   PRIMARY KEY (`id`),
   KEY `utente_id` (`utente_id`),
   CONSTRAINT `annuncio_ibfk_1` FOREIGN KEY (`utente_id`) REFERENCES `utente` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -50,7 +50,7 @@ CREATE TABLE `annuncio` (
 
 LOCK TABLES `annuncio` WRITE;
 /*!40000 ALTER TABLE `annuncio` DISABLE KEYS */;
-INSERT INTO `annuncio` VALUES (1,1,1,'2025-01-02 09:00:00',500.00,'Camera singola vicino al centro.',1,20,2,'via nervi 10, cesena','attivo','Appartamento1.jpeg','Appartamento1.jpeg, Appartamento2.jpeg'),(2,2,2,'2023-12-12 11:30:00',800.00,'Appartamento bilocale arredato.',2,60,1,'Via Cerchia di Sant\'Egidio, 807, Cesena','attivo','Appartamento2.jpeg','Appartamento1.jpeg, Appartamento2.jpeg'),(3,1,3,'2023-12-05 15:00:00',300.00,'Posto letto in stanza doppia.',1,15,3,'Via Portofino, 280, Cesena','attivo','postoletto.jpeg','Appartamento1.jpeg, Appartamento2.jpeg'),(25,17,1,'2025-01-16 18:18:45',0.00,'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',0,3,0,'AAAAAAAAAAAAAAAAAAAAAAAA','attivo','1737047925302-DJI_0840.JPG',NULL),(26,19,4,'2025-01-17 09:22:13',0.00,'bbbbbbbbbbbbbbbbb',0,5,0,'gggggg','attivo','1737102133274-DJI_0839.JPG',NULL);
+INSERT INTO `annuncio` VALUES (1,1,1,'2025-01-02 09:00:00',500.00,'Camera singola vicino al centro.',1,20,2,'via nervi 10, cesena','attivo','Appartamento1.jpeg','Appartamento1.jpeg, Appartamento2.jpeg'),(2,2,2,'2023-12-12 11:30:00',800.00,'Appartamento bilocale arredato.',2,60,1,'Via Cerchia di Sant\'Egidio, 807, Cesena','attivo','Appartamento2.jpeg','Appartamento1.jpeg, Appartamento2.jpeg'),(3,1,3,'2023-12-05 15:00:00',300.00,'Posto letto in stanza doppia.',1,15,3,'Via Portofino, 280, Cesena','attivo','postoletto.jpeg','Appartamento1.jpeg, Appartamento2.jpeg'),(25,17,4,'2025-01-16 18:18:45',500.00,'Ampio appartamento in zona Ippodromo',5,60,0,'Via L.C. Farini, 380','attivo','1737047925302-DJI_0840.JPG',NULL);
 /*!40000 ALTER TABLE `annuncio` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -77,7 +77,7 @@ CREATE TABLE `annuncioservizio` (
 
 LOCK TABLES `annuncioservizio` WRITE;
 /*!40000 ALTER TABLE `annuncioservizio` DISABLE KEYS */;
-INSERT INTO `annuncioservizio` VALUES (1,1),(1,2),(2,1),(2,3),(3,1),(3,4),(25,6),(26,1);
+INSERT INTO `annuncioservizio` VALUES (1,1),(1,2),(2,1),(2,3),(3,1),(3,4),(25,6);
 /*!40000 ALTER TABLE `annuncioservizio` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,7 +107,7 @@ CREATE TABLE `dettagli_annuncio` (
 
 LOCK TABLES `dettagli_annuncio` WRITE;
 /*!40000 ALTER TABLE `dettagli_annuncio` DISABLE KEYS */;
-INSERT INTO `dettagli_annuncio` VALUES (1,1,1,6,12),(2,2,2,12,24),(3,3,2,3,6),(25,1,0,0,0),(26,3,0,0,0);
+INSERT INTO `dettagli_annuncio` VALUES (1,1,1,6,12),(2,2,2,12,24),(3,3,2,3,6),(25,1,0,0,0);
 /*!40000 ALTER TABLE `dettagli_annuncio` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -134,7 +134,7 @@ CREATE TABLE `news` (
 
 LOCK TABLES `news` WRITE;
 /*!40000 ALTER TABLE `news` DISABLE KEYS */;
-INSERT INTO `news` VALUES (1,'Nuovi appartamenti disponibili!','Scopri le ultime offerte vicino al centro.','2023-12-20 10:00:00','news1.jpeg'),(2,'Promozione Natale','Sconti fino al 20% su alcune stanze per studenti.','2023-12-18 09:30:00','news2.jpeg'),(3,'Da oggi StudentHome è anche su mobile!','Siamo entusiasti di annunciare una grande novità:\n\nStudentHome è finalmente disponibile anche su dispositivi mobile! \n\nLa nostra piattaforma è stata ottimizzata per offrirti un’esperienza semplice, fluida e accessibile direttamente dal tuo smartphone o tablet.\n\nChe tu sia in viaggio, in pausa tra le lezioni o comodamente seduto sul divano, potrai cercare appartamenti o camere in affitto con la stessa facilità della versione desktop. Navigare tra gli annunci, salvare le tue opzioni preferite e contattare i proprietari non è mai stato così comodo e veloce.\n\nNon importa dove ti trovi: con StudentHome la tua nuova casa è sempre a portata di mano. Prova subito la nostra versione mobile e scopri quanto è facile trovare il tuo prossimo alloggio!','2025-01-13 08:08:08','news3.jpg'),(4,'Qualche imperdibile notizia!','StudentHome è lieta di annunciare una nuova partnership con il Cinema Aurora! \n\nA partire da oggi, tutti gli studenti registrati sulla nostra piattaforma potranno beneficiare di uno sconto del 20% sui biglietti per i film in programmazione.\n\nChe tu voglia rilassarti dopo una lunga giornata di studio o organizzare una serata con i coinquilini, questa è l’occasione perfetta per vivere momenti di svago a prezzi vantaggiosi.\n\nNon dimenticare di mostrare il tuo badge digitale StudentHome alla cassa per ottenere lo sconto.\n\nBuona visione!','2025-01-13 08:08:08','news4.jpg');
+INSERT INTO `news` VALUES (1,'Nuovi appartamenti disponibili!','Nel centro storico di Cesena, sono disponibili gli ultimi appartamenti, un’occasione imperdibile per chi sogna di vivere in una zona ricca di storia e comodità. Le soluzioni abitative offrono finiture moderne, efficienza energetica e, in alcuni casi, splendide viste panoramiche sulla città.\n\nVivere in centro significa avere tutto a portata di mano: negozi, ristoranti, cultura e servizi, il tutto in un contesto esclusivo e ben collegato.\n\nNon lasciarti sfuggire questa opportunità unica! Contatta subito le agenzie immobiliari per una visita.','2023-12-20 10:00:00','news1.jpeg'),(2,'Promozione Inverno','L\'inverno porta con sé grandi opportunità di risparmio grazie alla nuova promozione invernale! Per un periodo limitato, puoi approfittare di sconti esclusivi su una vasta gamma di prodotti, dai capi d\'abbigliamento agli articoli per la casa.\n\nNon lasciarti sfuggire l’occasione di rinnovare il tuo guardaroba o di acquistare quel prodotto che desideravi da tempo, a prezzi imperdibili.\n\nCorri nei negozi aderenti o visita i loro shop online: l’inverno non è mai stato così conveniente!','2023-12-18 09:30:00','news2.jpeg'),(3,'Da oggi StudentHome è anche su mobile!','Siamo entusiasti di annunciare una grande novità:\n\nStudentHome è finalmente disponibile anche su dispositivi mobile! \n\nLa nostra piattaforma è stata ottimizzata per offrirti un’esperienza semplice, fluida e accessibile direttamente dal tuo smartphone o tablet.\n\nChe tu sia in viaggio, in pausa tra le lezioni o comodamente seduto sul divano, potrai cercare appartamenti o camere in affitto con la stessa facilità della versione desktop. Navigare tra gli annunci, salvare le tue opzioni preferite e contattare i proprietari non è mai stato così comodo e veloce.\n\nNon importa dove ti trovi: con StudentHome la tua nuova casa è sempre a portata di mano. Prova subito la nostra versione mobile e scopri quanto è facile trovare il tuo prossimo alloggio!','2025-01-13 08:08:08','news3.jpg'),(4,'Qualche imperdibile notizia!','StudentHome è lieta di annunciare una nuova partnership con il Cinema Aurora! \n\nA partire da oggi, tutti gli studenti registrati sulla nostra piattaforma potranno beneficiare di uno sconto del 20% sui biglietti per i film in programmazione.\n\nChe tu voglia rilassarti dopo una lunga giornata di studio o organizzare una serata con i coinquilini, questa è l’occasione perfetta per vivere momenti di svago a prezzi vantaggiosi.\n\nNon dimenticare di mostrare il tuo badge digitale StudentHome alla cassa per ottenere lo sconto.\n\nBuona visione!','2025-01-13 08:08:08','news4.jpg');
 /*!40000 ALTER TABLE `news` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -161,7 +161,6 @@ CREATE TABLE `preferiti` (
 
 LOCK TABLES `preferiti` WRITE;
 /*!40000 ALTER TABLE `preferiti` DISABLE KEYS */;
-INSERT INTO `preferiti` VALUES (19,26);
 /*!40000 ALTER TABLE `preferiti` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -178,7 +177,7 @@ CREATE TABLE `quartierezona` (
   `descrizione` text DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nome_quartiere` (`nome_quartiere`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -187,7 +186,7 @@ CREATE TABLE `quartierezona` (
 
 LOCK TABLES `quartierezona` WRITE;
 /*!40000 ALTER TABLE `quartierezona` DISABLE KEYS */;
-INSERT INTO `quartierezona` VALUES (1,'Centro','Quartiere centrale con tutti i servizi a portata di mano.'),(2,'Oltresavio','Zona residenziale tranquilla con ottimi collegamenti.'),(3,'Valle Savio','Area immersa nel verde, ideale per chi cerca pace.'),(4,'Ippodromo','Zona vicino all\'ippodromo');
+INSERT INTO `quartierezona` VALUES (1,'Centro','Quartiere centrale con tutti i servizi a portata di mano.'),(2,'Oltresavio','Zona residenziale tranquilla con ottimi collegamenti.'),(3,'Valle Savio','Area immersa nel verde, ideale per chi cerca pace.'),(4,'Ippodromo','Zona vicino all\'ippodromo'),(5,'Fiorenzuola','Situato a sud del centro, include aree residenziali e commerciali.'),(6,'Cesuola','Quartiere collinare e residenziale che si estende a ovest del centro storico.'),(7,'Vigne','Quartiere popoloso con diverse infrastrutture, parchi e aree residenziali.\n\n'),(8,'Ravennate','Comprende aree a nord della città, lungo la strada verso Ravenna.'),(9,'Rubicone','Zona situata a nord-est verso il fiume Rubicone.');
 /*!40000 ALTER TABLE `quartierezona` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -203,7 +202,7 @@ CREATE TABLE `servizio` (
   `nome_servizio` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nome_servizio` (`nome_servizio`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -212,7 +211,7 @@ CREATE TABLE `servizio` (
 
 LOCK TABLES `servizio` WRITE;
 /*!40000 ALTER TABLE `servizio` DISABLE KEYS */;
-INSERT INTO `servizio` VALUES (4,'Aria Condizionata'),(3,'Ascensore'),(5,'Garage'),(7,'Netflix'),(2,'Riscaldamento'),(6,'Sky'),(1,'Wi-Fi');
+INSERT INTO `servizio` VALUES (4,'Aria Condizionata'),(3,'Ascensore'),(9,'Asciugatrice'),(10,'Forno'),(5,'Garage'),(11,'Lavastoviglie'),(8,'Lavatrice'),(7,'Netflix'),(2,'Riscaldamento autonomo'),(6,'Sky'),(1,'Wi-Fi');
 /*!40000 ALTER TABLE `servizio` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -281,4 +280,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-18 12:19:39
+-- Dump completed on 2025-01-19 16:19:06
